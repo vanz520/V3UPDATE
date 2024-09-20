3b4c1037a00d52de9ca23abf7ebedcc42c8f2124
vanz520 <alexanderdevano520@gmail.com>
2024-09-20 19:44:39 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ from config import (
     APP_ID,
     CHANNEL_ID,
     FORCE_SUB_CHANNEL,
+    FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
     LOGGER,
     OWNER,
@@ -49,6 +50,30 @@ class Bot(Client):
             sys.exit()
 
         if FORCE_SUB_CHANNEL:
+            try:
+                info = await self.get_chat(FORCE_SUB_CHANNEL)
+                link = info.invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                    link = info.invite_link
+                self.invitelink = link
+                self.LOGGER(__name__).info(
+                    f"FORCE_SUB_CHANNEL detected!\n┌ Title: {info.title}\n└ Chat ID: {info.id}\n——"
+                )
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning(
+                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_CHANNEL!"
+                )
+                self.LOGGER(__name__).warning(
+                    f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
+                )
+                self.LOGGER(__name__).info(
+                    "Bot Berhenti. Gabung Group https://t.me/SharingUserbot untuk Bantuan"
+                )
+                sys.exit()
+
+         if FORCE_SUB_CHANNEL:
             try:
                 info = await self.get_chat(FORCE_SUB_CHANNEL)
                 link = info.invite_link
